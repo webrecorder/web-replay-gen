2171e2f8e3378b11304aafe1596a220bb51cd7ff
sua yoo <sua@suayoo.com>
2022-08-23 14:44:13 -0700

    add lit

---
+++ src/components/wrg-replay.js
@@ -0,0 +1,16 @@
+import {
+  html,
+  css,
+  LitElement,
+} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+
+customElements.define(
+  'wrg-replay',
+  class extends LitElement {
+    static properties = {};
+
+    render() {
+      return html`TODO`;
+    }
+  }
+);
