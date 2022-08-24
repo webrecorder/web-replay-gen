b2e34f9f9c88b3597a8b1ee2740f2e6c54343a08
sua yoo <sua@suayoo.com>
2022-08-24 11:44:20 -0700

    support archive name

--- src/_data/archivePages.js
+++ src/_data/archivePages.js
@@ -1,9 +1,36 @@
 const wrgConfig = require('../../getConfig')();
 
-module.exports = () =>
-  wrgConfig.archives.map((url) => {
-    return {
-      name: url,
-      url: `/archive/#/${encodeURIComponent(url)}`,
-    };
-  });
+/**
+ * Transform archives data to site page data
+ * @param {string|Object} data - WACZ data, can be URL string or { name: string; url: string }
+ * @returns {Object} archive
+ * @returns {string} archive.name - Name of archive
+ * @returns {string} archive.waczURL - URL to WACZ file
+ * @returns {string} archive.pathname - Site page path
+ */
+function mapToPage(data, idx) {
+  let name, waczURL;
+
+  if (typeof data === 'string') {
+    name = data;
+    waczURL = data;
+  } else if (
+    typeof data === 'object' &&
+    data.constructor === Object &&
+    data.name &&
+    data.url
+  ) {
+    name = data.name;
+    waczURL = data.url;
+  }
+
+  if (name && waczURL) {
+    const pathname = `/archive/#/${encodeURIComponent(waczURL)}`;
+
+    return { name, waczURL, pathname };
+  }
+
+  console.warn(`Invalid WACZ data at index ${idx}, skipping`);
+}
+
+module.exports = () => wrgConfig.archives.map(mapToPage).filter((v) => v);
