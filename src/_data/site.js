1638a9c8188bac739f13b0230f3dd20bac869c60
sua yoo <sua@suayoo.com>
2022-08-25 12:03:34 -0700

    log origin url

--- src/_data/site.js
+++ src/_data/site.js
@@ -5,16 +5,19 @@ const path = require('path');
 
 const site = { ...wrgConfig.site };
 
+const hasGithubPagesWorkflow = existsSync(
+  path.resolve(
+    process.env.ELEVENTY_ROOT,
+    '.github/workflows/publish-gh-pages.yml'
+  )
+);
+
 if (site.url) {
   site.url = site.url.replace(/\/+$/, '');
 } else {
-  const hasGithubPagesWorkflow = existsSync(
-    path.resolve(
-      process.env.ELEVENTY_ROOT,
-      '.github/workflows/publish-gh-pages.yml'
-    )
-  );
   const originURL = execSync('git config --get remote.origin.url').toString();
+
+  console.log('originURL:', originURL);
 
   if (hasGithubPagesWorkflow && originURL.startsWith('git@github.com:')) {
     const [org, repo] = originURL
@@ -24,11 +27,13 @@ if (site.url) {
       .split('/');
 
     site.url = `https://${org}.github.io/${repo}`;
-
-    console.log(`This website will deploy to GitHub Pages at ${site.url}`);
   }
 
   site.url = site.url || '';
+}
+
+if (hasGithubPagesWorkflow) {
+  console.log(`This website will deploy to GitHub Pages (${site.url})`);
 }
 
 module.exports = site;
