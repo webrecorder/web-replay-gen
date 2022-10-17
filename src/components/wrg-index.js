a16b4632480217b7d07d3926c381a7e469974878
sua yoo <sua@suayoo.com>
2022-10-17 13:43:38 -0700

    convert list

---
+++ src/components/wrg-index.js
@@ -0,0 +1,71 @@
+import { html, css, LitElement } from 'lit';
+
+/**
+ * List archives
+ */
+customElements.define(
+  'wrg-index',
+  class extends LitElement {
+    static properties = {
+      archives: {
+        type: Array,
+      },
+    };
+
+    static styles = css`
+      ul {
+        border: var(--sl-panel-border-width) solid var(--sl-panel-border-color);
+        border-radius: var(--sl-border-radius-medium);
+        margin: 0;
+        padding: 0;
+        list-style: none;
+      }
+
+      li:not(:first-child) {
+        border-top: var(--sl-panel-border-width) solid
+          var(--sl-panel-border-color);
+      }
+
+      a {
+        display: block;
+        padding: var(--sl-spacing-small);
+        line-height: 1;
+        color: var(--sl-color-neutral-700);
+        text-decoration: none;
+      }
+
+      a:hover {
+        background-color: var(--sl-color-neutral-50);
+      }
+    `;
+
+    render() {
+      return html`
+        <ul>
+          ${this.archives.map(
+            (page) => html`
+              <li>
+                <a href=${page.pathname}>${page.name}</a>
+              </li>
+            `
+          )}
+        </ul>
+      `;
+      // return html`
+      //   <ul>
+      //     ${this.archives.map(
+      //       (page) => html`
+      //         <li class="border-t first:border-t-0">
+      //           <a
+      //             href="{{ page.pathname }}"
+      //             class="block p-2 text-neutral-700 hover:bg-neutral-50 hover:text-neutral-900"
+      //             >${page.name}</a
+      //           >
+      //         </li>
+      //       `
+      //     )}
+      //   </ul>
+      // `;
+    }
+  }
+);
